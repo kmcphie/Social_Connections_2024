8e13d24bb4ee453d50a5975441278be08aa2459c
Katherine McPhie <katherinemcphie@college.harvard.edu>
2020-10-16 22:43:24 -0400

    updated about tab

--- shiny_app/ui.R
+++ shiny_app/ui.R
@@ -38,71 +38,104 @@ ui <- navbarPage(
               br(),
               plotOutput("location_dist"),
               br(),
-              plotOutput("gender_dist")
+              plotOutput("gender_dist"),
+              br(),
+              br()
              ))
-           )
+            )
   ),
   
   ########## SECOND PAGE ##########
   
   tabPanel("The Social Web",
-           h2(tags$b("Total Sample Size")),
-           p(
-             "Information about the people who answered our survey and a graph
-             showing how they are connected will go on this page."
+           fluidPage(
+             br(),
+             br(),
+             fluidRow(column(1), column(10,
+              h2(tags$b("Total Sample Size")),
+              p(
+                "Information about the people who answered our survey and a graph
+                showing how they are connected will go on this page."
+              ),
+              br(),
+              br()
+            ))
            )
   ),
   
   ########## THIRD PAGE ##########
   
   tabPanel("Analyzing the Data",
-           h2(tags$b("[Insert Title Here]")),
-           p(
-             "We will analyze our survey results on this page."
+           fluidPage(
+             br(),
+             br(),
+             fluidRow(column(1), column(10,
+              h2(tags$b("[Insert Title Here]")),
+              p(
+                "We will analyze our survey results on this page."
+              ),
+              br(),
+              br()
+             ))
            )
   ),
   
   ########## FOURTH PAGE ##########
   
   tabPanel("About", 
-           h2(tags$b("About Our Project")),
-           p(
-             "This was a final project for",
-             a("Gov 50: Data", href="https://www.davidkane.info/files/gov_50_fall_2020.html"),
-             "at Harvard College."
-           ),
-           p(
-             "This project's GitHub repository lives",
-             a("here", href="https://github.com/kmcphie/Social_Connections_2024"),
-             "."
-           ),
-           h2(tags$b("About Our Team")),
-           fluidRow(
-             column(3, imageOutput("elliott")),
-             column(4, offset = 1, 
-                    h3(tags$b("Person")), br(), 
-                    p("Hi, I'm a placeholder!")),
-           ),
-           br(),
-           fluidRow(
-             column(3, imageOutput("ava")),
-             column(4, offset = 1, 
-                    h3(tags$b("Person")), br(), 
-                    p("Hi, I'm a placeholder!")),
-           ),
-           br(),
-           fluidRow(
-             column(3, imageOutput("gio")),
-             column(4, offset = 1, 
-                    h3(tags$b("Person")), br(), 
-                    p("Hi, I'm a placeholder!")),
-           ),
-           br(),
-           fluidRow(
-             column(3, imageOutput("katherine")),
-             column(4, offset = 1, 
-                    h3(tags$b("Person")), br(), 
-                    p("Hi, I'm a placeholder!")),
+           fluidPage(
+             br(),
+             br(),
+             fluidRow(column(1), column(10,
+              h2(tags$b("About Our Project"),
+              align = "center"),
+              p(
+                "This was a final project for",
+                a("Gov 50: Data", href="https://www.davidkane.info/files/gov_50_fall_2020.html"),
+                "at Harvard College.",
+                align = "center"
+              ),
+              p(
+                "This project's GitHub repository lives",
+                a("here", href="https://github.com/kmcphie/Social_Connections_2024"),
+                ".",
+                align = "center"
+              ),
+              br(),
+              h2(tags$b("About Our Team"),
+                 align = "center")
+             )),
+             fluidRow(column(3), column(9,
+              fluidRow(
+                column(5, imageOutput("elliott")),
+                column(5, offset = 0, 
+                       h3(tags$b("Person")), br(), 
+                       p("Hi, I'm a placeholder!")),
+              ),
+              br(),
+              fluidRow(
+                column(5, imageOutput("ava")),
+                column(5, offset = 0, 
+                       h3(tags$b("Person")), br(), 
+                       p("Hi, I'm a placeholder!")),
+              ),
+              br(),
+              fluidRow(
+                column(5, imageOutput("gio")),
+                column(5, offset = 0, 
+                       h3(tags$b("Person")), br(), 
+                       p("Hi, I'm a placeholder!")),
+              ),
+              br(),
+              fluidRow(
+                column(5, imageOutput("katherine")),
+                column(5, offset = 0, 
+                       h3(tags$b("Person")), br(), 
+                       p("Hi, I'm a placeholder!")),
+              ),
+              br(),
+              br()
+             ))
            )
   )
 )
