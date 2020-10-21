5a584fdab007dcb0d69c60e9bbbb08ee86ff6e5f
AvaSwanson <avaswanson@dhcp-10-250-98-141.harvard.edu>
2020-10-21 12:28:34 -0400

    Network Practice

--- shiny_app/Network_Practice.R
+++ shiny_app/Network_Practice.R
@@ -7,7 +7,15 @@ library(shiny)
 library(shinythemes)
 library(readxl)
 
+responses <- readRDS("responses.rds")
+
+# Home/Desktop/Gov50Projects/Social_Connections_2024/shiny_app/
+
+
+actorNetwork <- graph_from_data_frame(d=movies, vertices=actors, directed=F)
+
 # RPubs Walk Through ------------------------------------------------------
+
 
 actors <- read_csv("https://raw.githubusercontent.com/OPER682-Tucker/Social-Network-Analysis/master/Actors.csv",
                    col_type = cols(
@@ -66,10 +74,71 @@ plot(actorNetwork)
 #        col=c("green","black","orange"), lty=1, cex=.8)
 
 
-responses <- readRDS("responses.rds")
+# Star Wars Example -------------------------------------------------------
 
-# Home/Desktop/Gov50Projects/Social_Connections_2024/shiny_app/
 
-excel_responses <- read_excel("shiny_app/raw_data/survey_responses.xlsx")
+# http://pablobarbera.com/big-data-upf/html/02a-networks-intro-visualization.html
 
-actorNetwork <- graph_from_data_frame(d=movies, vertices=actors, directed=F)
+edges <- read.csv("data/star-wars-network-edges.csv")
+head(edges)
+
+nodes <- read.csv("data/star-wars-network-nodes.csv")
+head(nodes)
+
+# We can use the graph_from_data_frame function, which takes two arguments: d, 
+# the data frame with the edge list in the first two columns; and vertices, a 
+# data frame with node data with the node label in the first column. (Note that 
+# igraph calls the nodes vertices, but it’s exactly the same thing.)
+
+g <- graph_from_data_frame(d=edges, vertices=nodes, directed=FALSE)
+g
+
+# Use graph_from_data_frame and then plot() to plot it 
+
+
+# Jeremiah's Code ---------------------------------------------------------
+
+
+output$mark_plot <- renderVisNetwork({
+  nodes2 <- read_csv("data/nodes2.csv")
+  edges2 <- read_csv("data/edges2.csv")
+  
+  # Changed shape, color, and size for each group
+  
+  visNetwork(nodes2, edges2) %>%
+    visGroups(groupname = "Dorms", color = "darkblue", shape = "square", size = 65) %>%
+    visGroups(groupname = "Pre-Orientation", color = "darkred", shape = "square", size = 45) %>%
+    visGroups(groupname = "Sports", color = "darkgreen", shape = "square", size = 45) %>%
+    
+    # Added functionality to highlight close connections when hovering over node
+    
+    visOptions(nodesIdSelection = list(enabled = TRUE,
+                                       style = "margin-bottom: -30px; visibility: hidden"),
+               highlightNearest = list(enabled = T, degree = 2, hover = T),
+               selectedBy = "group") %>%
+    
+    # Adjusted physics to decrease load time
+    
+    visPhysics(
+      solver = "forceAtlas2Based", 
+      timestep = 0.5,
+      minVelocity = 1,
+      maxVelocity = 30,
+      forceAtlas2Based = list(gravitationalConstant = -200, damping = 1),
+      stabilization = list(iterations = 300, updateInterval = 10),
+      adaptiveTimestep = TRUE) %>%
+    
+    # Added legend for groups
+    
+    visLegend(zoom = FALSE) %>%
+    
+    # Disabled graph movement within window
+    
+    visInteraction(dragView = FALSE, 
+                   zoomView = FALSE,
+                   navigationButtons = TRUE)
+  
+  
+})
+
+f
